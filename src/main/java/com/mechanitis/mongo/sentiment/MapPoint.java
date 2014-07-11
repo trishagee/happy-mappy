b8eb7252f0b3de2c4fb5fd6a80e164cec28f93e6
Trisha Gee <trishagee@github.com>
2014-07-11 21:43:45 +0200

    Changed MapPoint to a simpler shape for the UI

--- src/main/java/com/mechanitis/mongo/sentiment/MapPoint.java
+++ src/main/java/com/mechanitis/mongo/sentiment/MapPoint.java
@@ -3,55 +3,36 @@ package com.mechanitis.mongo.sentiment;
 import com.mechanitis.mongo.sentiment.processor.HappyStatus;
 import com.mechanitis.mongo.sentiment.processor.SadStatus;
 import com.mechanitis.mongo.sentiment.twitter.RawStatus;
-import twitter4j.GeoLocation;
 
 public class MapPoint {
     private final String feeling;
-    private Location location;
+    private double[] location = new double[2];
     private long id;
 
     public MapPoint(final HappyStatus happyStatus) {
         this(happyStatus.getStatus(), "happy");
     }
+
     public MapPoint(final SadStatus sadStatus) {
         this(sadStatus.getStatus(), "sad");
     }
 
     MapPoint(final RawStatus status, String feeling) {
         this.feeling = feeling;
-        this.location = new Location(status.getGeoLocation());
         this.id = status.getId();
+        this.location[0] = status.getGeoLocation().getLongitude();
+        this.location[1] = status.getGeoLocation().getLatitude();
     }
 
     public long getId() {
         return id;
     }
 
-    public Location getLocation() {
+    public double[] getLocation() {
         return location;
     }
 
     public String getFeeling() {
         return feeling;
-    }
-
-    private static class Location {
-        private double[] coordinates = new double[2];
-        private String type;
-
-        public Location(final GeoLocation geoLocation) {
-            System.out.println(geoLocation.getLongitude());
-            this.coordinates[0] = geoLocation.getLongitude();
-            this.coordinates[1] = geoLocation.getLatitude();
-            this.type = "point";
-        }
-
-        public double[] getCoordinates() {
-            return coordinates;
-        }
-
-        public String getType() {
-            return type;
-        }
     }
 }
